619682de3575347584b394a7042ed8e8380dd544
taskinoz <tristan@taskinoz.com>
2019-05-14 23:24:33 +1000

    Reconfigured to be a npm module, added undefuined checks and basic JSON checks

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
-const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('config.json','utf8'));
-var file = '<?php\n\n';
+module.exports = function phpReq(input) {
 
-if ((config.get).length>0) {
-  file+='\t//Get requests\n\tif (';
-  for (let i = 0; i < (config.get).length; i++) {
-    file+=`isset($_GET['${config.get[i]}'])`;
-    if (i<(config.get).length-1) {
-      file+=" && ";
+  function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return str;
     }
-  }
-  file+="){\n";
-  for (let i = 0; i < (config.get).length; i++) {
-    file+=`\t\t$api${(config.get[i]).charAt(0).toUpperCase()+(config.get[i]).slice(1)} = isset($_GET['${config.get[i]}']);\n`;
-  }
-  file+="\t}\n\n"
+    return JSON.parse(str);
 }
-if ((config.post).length>0) {
-  file+='\t//Post requests\n\tif (';
-  for (let i = 0; i < (config.post).length; i++) {
-    file+=`isset($_POST['${config.post[i]}'])`;
-    if (i<(config.post).length-1) {
-      file+=" && ";
+
+  const config = isJson(input);
+
+  // Check for filename. If it exist write to a file
+  if (config.name !== undefined) {
+    var fs = require('fs');
+    var file = '<?php\n\n';
+  }
+  else {
+    var file="";
+  }
+
+  if (config.get !== undefined && (config.get).length>0) {
+    file+='//Get requests\n\tif (';
+    for (let i = 0; i < (config.get).length; i++) {
+      file+=`isset($_GET['${config.get[i]}'])`;
+      if (i<(config.get).length-1) {
+        file+=" && ";
+      }
     }
+    file+="){\n";
+    for (let i = 0; i < (config.get).length; i++) {
+      file+=`\t\t$api${(config.get[i]).charAt(0).toUpperCase()+(config.get[i]).slice(1)} = isset($_GET['${config.get[i]}']);\n`;
+    }
+    file+="\t}\n\n";
   }
-  file+="){\n";
-  for (let i = 0; i < (config.post).length; i++) {
-    file+=`\t\t$api${(config.post[i]).charAt(0).toUpperCase()+(config.post[i]).slice(1)} = isset($_GET['${config.post[i]}']);\n`;
+  if (config.post !== undefined && (config.post).length>0) {
+    file+='//Post requests\n\tif (';
+    for (let i = 0; i < (config.post).length; i++) {
+      file+=`isset($_POST['${config.post[i]}'])`;
+      if (i<(config.post).length-1) {
+        file+=" && ";
+      }
+    }
+    file+="){\n";
+    for (let i = 0; i < (config.post).length; i++) {
+      file+=`\t\t$api${(config.post[i]).charAt(0).toUpperCase()+(config.post[i]).slice(1)} = isset($_GET['${config.post[i]}']);\n`;
+    }
+    file+="\t}\n\n";
   }
-  file+="\t}\n\n"
+
+  if (file.includes("<?php")) {
+    fs.writeFileSync(`${config.name}.php`, file);
+    return `File ${config.name}.php created`;
+  }
+  else {
+    return file;
+  }
 }
-fs.writeFileSync(`${config.name}.php`,file);
-/*
-if (isset($_GET['temp'])){
-  $switch = $_GET['temp'];
-}
-else{
-  $switch = rand(0,5);
-}
-*/
